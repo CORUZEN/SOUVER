a6bb69e6091ca8912b584f29cd693be8868cfb11
CORUZEN <229539221+CORUZEN@users.noreply.github.com>
2026-04-23 07:44:03 -0300

    feat: update app version to v1.00.426 in service worker and generated version file

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Ouro Verde PWA Service Worker
-const CACHE_VERSION = 'ov-pwa-v1.00.419'
+const CACHE_VERSION = 'ov-pwa-v1.00.426'
 const STATIC_CACHE = `${CACHE_VERSION}-static`
 const API_CACHE = `${CACHE_VERSION}-api`
 
